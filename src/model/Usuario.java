0269480ff9cdd09a55da72e91d449d449a332d19
LuiF <95049792+LuiFyt@users.noreply.github.com>
2024-11-04 11:08:56 -0300

    Add files via upload

---
+++ src/model/Usuario.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author luiz.faraujo
+ */
+public class Usuario {
+    
+    private int id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String telefone;
+    private Endereco endereco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+    public boolean login(String senha) {
+        
+    }
+}
