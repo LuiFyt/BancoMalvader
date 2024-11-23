3fc557a8957fb18ecb89914486ab7462389ef24d
Henrique de Oliveira Cruz <162234935+RickOliverRatas@users.noreply.github.com>
2024-11-23 10:59:23 -0300

    Add files via upload

--- src/model/ContaCorrente.java
+++ src/model/ContaCorrente.java
@@ -3,9 +3,16 @@ package model;
 import java.time.LocalDate;
 
 public class ContaCorrente extends Conta {
-
     private double limite;
     private LocalDate dataVencimento;
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
 
     public LocalDate getDataVencimento() {
         return dataVencimento;
@@ -15,11 +22,7 @@ public class ContaCorrente extends Conta {
         this.dataVencimento = dataVencimento;
     }
 
-    public double getLimite() {
+    public double consultarLimite() {
         return limite;
-    }
-
-    public void setLimite(double limite) {
-        this.limite = limite;
     }
 }
